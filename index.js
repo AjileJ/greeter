bb47d8b181c6e08b5d3915feee0b530854046b23
AjileJ <53097022+AjileJ@users.noreply.github.com>
2019-10-31 10:48:10 -0700

    add

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
-const express = require('express');
-
+const express = require('express')
+require('dotenv').config();;
 const server = express(); // creating an instance of an express server
 
 server.get('/', (req, res) => {
   res.status(200).json({ hello: 'Web 23' });
 });
 
-server.listen(4000, () => {
+
+
+
+const port  = process.env.PORT || 4000;
+
+console.log('port', process.env.PORT)
+
+server.listen(port, () => {
   console.log('\n Server running on port 4000 \n');
 });
